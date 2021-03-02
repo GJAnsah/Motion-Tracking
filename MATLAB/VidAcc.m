59e9319ddadec53020ceee2335e609d4a7a46332
Gilgal Ansah <ansahgilgaljustice@gmail.com>
2021-03-02 01:00:56 -0700

    Update VidAcc.m

--- MATLAB/VidAcc.m
+++ MATLAB/VidAcc.m
@@ -1,18 +1,24 @@
+%Author: Gilgal Ansah
+% The program tracks a green marker across video frames and plots the displacement, velocity, and acceleration.
 
-videoSource = VideoReader('MVI_4091.MP4');
-videoSource.CurrentTime=30;
+
+%input video file
+videoSource = VideoReader('free-fall2.mp4');
+%video frames are shown at 30fps.
+
+%videoSource.CurrentTime=30;
 videoPlayer = vision.VideoPlayer();
 T=[];
 W=[];
 C1=[];
 C2=[];
 CC=[];
+
+%track marker for each frame
 while hasFrame(videoSource)
      x  = readFrame(videoSource);
      frame=x;
-     % Taking each picture element in the matrix across each row, we determine
-% if it has rbg value with red value less than Rmin or green value more than
-%  Gmax or blue value more than Bmax and make it white if its true
+     %making all pixels white except for green pixels. 
      if size(x,3)<3
         for e=size(x)+1:3
             x(:,:,e)=zeros(size(x,1),size(x,2));
@@ -25,18 +31,27 @@ while hasFrame(videoSource)
              end
          end
      end
-     %subtracting the grayscale image from the red channel
+     
+     %subtracting the grayscale image from the green channel
      x2 = imsubtract(x(:,:,2), rgb2gray(x));
+     
      %Use a median filter to filter out noise
 %      x2 = medfilt2(x2, [3 3]);
+
+     %removing small objects. 
      x2 = bwareaopen(x2,500);
+     
      %converting image to black and white
 %      x2 = im2bw(x2,0.18)
+
+     %labeling objects
      bw = bwlabel(x2);
-     %finding the boundaries of objects
+     
+     %finding the boundaries and centroids of objects
      stats = regionprops(bw, 'BoundingBox','Centroid');
  
      imshow(frame);
+     %drawing bounding box
 for object = 1:length(stats)
     bb = stats(object).BoundingBox;
     cc=stats(object).Centroid;
@@ -45,16 +60,19 @@ for object = 1:length(stats)
     disp(bb);
     
 end
-    T=[T,bb(1)];
-    W=[W,bb(3)];
-    CC=[CC,cc];
-    C1=[C1,cc(1)];
-    C2=[C1,cc(2)];
+    T=[T,bb(1)]; %x-coordinate of bounding box
+    W=[W,bb(3)]; %width of bounding box
+    CC=[CC,cc]; %centroid (x,y)
+    C1=[C1,cc(1)]; %x-coordinate of centroid
+    C2=[C1,cc(2)]; %y-coordinate of centroid
+    
     disp(length(T));
 %      videoPlayer(out);
      pause(0.1);
 end
 release(videoPlayer);
 %save('VidAcc_variables')
+
+%calculating velocity and acceleration
 Vt=diff(T)*30;
 At=diff(Vt)*30;
