5e403765770eb1a2a5fd857b59b9cfff29e8ef11
Gilgal Ansah <ansahgilgaljustice@gmail.com>
2021-03-31 02:32:34 -0700

    Update VidAcc.m

--- MATLAB/VidAcc.m
+++ MATLAB/VidAcc.m
@@ -1,78 +1,72 @@
-%Author: Gilgal Ansah
-% The program tracks a green marker across video frames and plots the displacement, velocity, and acceleration.
 
+videoSource = VideoReader(''); %insert videofile name here with file extension. eg videoSource = VideoReader('my_vid.mp4')
 
-%input video file
-videoSource = VideoReader('free-fall2.mp4');
-%video frames are shown at 30fps.
-
-%videoSource.CurrentTime=30;
+videoSource.CurrentTime= ;  %video time (seconds) to start tracking from.
 videoPlayer = vision.VideoPlayer();
+
 T=[];
-W=[];
 C1=[];
 C2=[];
 CC=[];
 
-%track marker for each frame
 while hasFrame(videoSource)
-     x  = readFrame(videoSource);
-     frame=x;
-     %making all pixels white except for green pixels. 
-     if size(x,3)<3
+    x  = readFrame(videoSource);
+    frame=x;
+    % Taking each picture element in the matrix across each row, we determine
+    % if it has rbg value with red value less than Rmin or green value more than
+    %  Gmax or blue value more than Bmax and make it white if its true
+    if size(x,3)<3
         for e=size(x)+1:3
             x(:,:,e)=zeros(size(x,1),size(x,2));
         end
-     end
-     for i=1:size(x,1)
-         for j=1:size(x,2)
-             if x(i,j,1)>90|| x(i,j,2)<70 || x(i,j,3)>50
-                 x(i,j,:)=[255 255 255];
-             end
-         end
-     end
-     
-     %subtracting the grayscale image from the green channel
-     x2 = imsubtract(x(:,:,2), rgb2gray(x));
-     
-     %Use a median filter to filter out noise
-%      x2 = medfilt2(x2, [3 3]);
-
-     %removing small objects. 
-     x2 = bwareaopen(x2,500);
-     
-     %converting image to black and white
-%      x2 = im2bw(x2,0.18)
-
-     %labeling objects
-     bw = bwlabel(x2);
-     
-     %finding the boundaries and centroids of objects
-     stats = regionprops(bw, 'BoundingBox','Centroid');
- 
-     imshow(frame);
-     %drawing bounding box
-for object = 1:length(stats)
-    bb = stats(object).BoundingBox;
-    cc=stats(object).Centroid;
-    rectangle('Position',bb,'EdgeColor','b','LineWidth',1)
-    disp('next bb')
-    disp(bb);
+    end
+    for i=1:size(x,1)
+        for j=1:size(x,2)
+            if x(i,j,1)>100|| x(i,j,2)<80 || x(i,j,3)>60
+                x(i,j,:)=[255 255 255];
+            end
+        end
+    end
+    %subtracting the grayscale image from the red channel
+    x2 = imsubtract(x(:,:,2), rgb2gray(x));
+    %Use a median filter to filter out noise
+    x2 = medfilt2(x2, [3 3]);
     
-end
-    T=[T,bb(1)]; %x-coordinate of bounding box
-    W=[W,bb(3)]; %width of bounding box
-    CC=[CC,cc]; %centroid (x,y)
-    C1=[C1,cc(1)]; %x-coordinate of centroid
-    C2=[C1,cc(2)]; %y-coordinate of centroid
+    %removing objects with few pixels
+    x2 = bwareaopen(x2,20);
     
+    %converting image to black and white
+    %      x2 = im2bw(x2,0.18);
+    bw = bwlabel(x2);
+    
+    %finding the boundaries of objects
+    stats = regionprops(bw, 'BoundingBox','Centroid');
+    
+    imshow(frame);
+    widths=[];
+    for object = 1:length(stats)
+        bb = stats(object).BoundingBox;
+        widths=[widths,bb(3)];
+    end
+    for object = 1:length(stats)
+        bb = stats(object).BoundingBox;
+        if bb(3) == max(widths)
+            cc=stats(object).Centroid;
+            rectangle('Position',bb,'EdgeColor','b','LineWidth',1)
+            disp('next bb')
+            disp(bb);
+            break
+        end 
+    end
+    T=[T,bb(1)];
+    CC=[CC,cc];
+    C1=[C1,cc(1)];
+    C2=[C1,cc(2)];
     disp(length(T));
-%      videoPlayer(out);
-     pause(0.1);
+    %      videoPlayer(out);
+    pause(0.1);
 end
 release(videoPlayer);
 %save('VidAcc_variables')
-
-%calculating velocity and acceleration
 Vt=diff(T)*30;
 At=diff(Vt)*30;
